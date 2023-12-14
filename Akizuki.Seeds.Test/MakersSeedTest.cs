5bb9358f328281168488d4c8cef0f6d157a2a6b4
danbara01703 <georgetech1984@gmail.com>
2023-12-14 14:07:50 +0900

    秋月電子のページからメーカー名を取得するSeedを実装

---
+++ Akizuki.Seeds.Test/MakersSeedTest.cs
@@ -0,0 +1,26 @@
+﻿using Akizuki.Domain.Catalogs;
+using CapStore.Domain.Makers;
+
+namespace Akizuki.Seeds.Test;
+
+public class MakersSeedTest
+{
+
+    private readonly MakersSeed _seed;
+
+    public MakersSeedTest()
+    {
+        _seed = new MakersSeed();
+    }
+
+    [Theory(DisplayName = "メーカー名取得")]
+    [Trait("Category", "Seeds")]
+    [InlineData("https://akizukidenshi.com/catalog/c/c/")]
+    public async Task SuccessTest(string url)
+    {
+        AkizukiPageUrl akizukiPageUrl = new AkizukiPageUrl(url);
+        IEnumerable<MakerName> makerNames = await _seed.FetchMakerNamesFromAkizukiPage(akizukiPageUrl);
+
+        Assert.True(makerNames.Any());
+    }
+}
---
+++ Akizuki.Seeds/Makers/MakersSeed.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Akizuki.Domain.Catalogs;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using CapStore.Domain.Makers;
+
+namespace Akizuki.Seeds;
+
+public class MakersSeed
+{
+    public MakersSeed() { }
+
+    /// <summary>
+    /// 秋月電子のページからメーカー名をすべて取得する
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public async Task<IEnumerable<MakerName>> FetchMakerNamesFromAkizukiPage(AkizukiPageUrl url)
+    {
+        using (IBrowsingContext context = BrowsingContext.New(Configuration.Default.WithDefaultLoader()))
+        {
+            IDocument document = await context.OpenAsync(url.Value);
+            HtmlParser parser = new HtmlParser();
+            IDocument parsedDocument = await parser.ParseDocumentAsync(document);
+
+            IHtmlCollection<IElement> elements = parsedDocument.GetElementsByClassName("menu03");
+            IEnumerable<MakerName> makerElements =
+                elements
+                    .Where(x => x.ChildElementCount > 0)
+                    .Select(x => x.Children.FirstOrDefault())
+                    .Where(x =>
+                    {
+                        string? href = x.GetAttribute("href");
+                        if (href == null) { return false; }
+                        return href.Contains("?maker=");
+                    })
+                    .Select(x => new MakerName(
+                        x.TextContent.Replace("\n", "").Replace("\t", "").Replace("　", "").Trim()
+                        )
+                    );
+
+            return makerElements;
+        }
+    }
+}
