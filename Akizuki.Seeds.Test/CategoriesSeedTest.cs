c6b7bd020c7bc17c4ef76cce0fd2e645dde49475
danbara01703 <georgetech1984@gmail.com>
2023-12-14 13:26:38 +0900

    秋月電子ページから大項目のカテゴリー名を取得するSeedを実装

---
+++ Akizuki.Seeds.Test/CategoriesSeedTest.cs
@@ -0,0 +1,27 @@
+﻿
+using Akizuki.Domain.Catalogs;
+using CapStore.Domain.Categories;
+
+namespace Akizuki.Seeds.Test;
+
+public class CategoriesSeedTest
+{
+    private readonly CategoriesSeed _seed;
+
+    public CategoriesSeedTest()
+    {
+        _seed = new CategoriesSeed();
+    }
+
+    [Theory(DisplayName = "カテゴリー取得")]
+    [Trait("Category", "Seeds")]
+    [InlineData("https://akizukidenshi.com/catalog/c/c/")]
+    public async Task SuccessTest(string url)
+    {
+        AkizukiPageUrl akizukiPageUrl = new AkizukiPageUrl(url);
+        IEnumerable<CategoryName> categoryNames = await _seed.FetchCategoryNamesFromAkizukiPage(akizukiPageUrl);
+
+        Assert.True(categoryNames.Any());
+        Assert.Equal(24, categoryNames.Count());
+    }
+}
---
+++ Akizuki.Seeds/Categories/CategoriesSeed.cs
@@ -0,0 +1,38 @@
+﻿using Akizuki.Domain.Catalogs;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using CapStore.Domain.Categories;
+
+namespace Akizuki.Seeds;
+
+public class CategoriesSeed
+{
+    public CategoriesSeed() { }
+
+    /// <summary>
+    /// 秋月電子のページから大項目のカテゴリー名をすべて取得する
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public async Task<IEnumerable<CategoryName>> FetchCategoryNamesFromAkizukiPage(AkizukiPageUrl url)
+    {
+        using (IBrowsingContext context = BrowsingContext.New(Configuration.Default.WithDefaultLoader()))
+        {
+            IDocument document = await context.OpenAsync(url.Value);
+            HtmlParser parser = new HtmlParser();
+            IDocument parsedDocument = await parser.ParseDocumentAsync(document);
+
+            IHtmlCollection<IElement> elements = parsedDocument.GetElementsByTagName("div");
+            IEnumerable<CategoryName> categoryElements =
+                elements
+                    .Where(x => x.Id == "cate-01" && x.InnerHtml.Contains("<a href="))
+                    .Select(x => new CategoryName(
+                        x.TextContent.Replace("\n", "").Replace("\t", "")
+                        )
+                    );
+
+            return categoryElements;
+        }
+    }
+}
