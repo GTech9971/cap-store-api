2ddc77f9618ed6854f17cb66ce4ea1c120a35431
George <georgetech1984@gmail.com>
2023-12-17 18:48:30 +0900

    秋月電子の注文IDモデルを実装

---
+++ Akizuki.Domain.Orders/OrderId.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using CapStore.Domain.Shareds.Exceptions;
+
+namespace Akizuki.Domain.Orders;
+
+/// <summary>
+/// 秋月電子で購入した際のID
+/// </summary>
+public class OrderId
+{
+
+    private const string PATTERN = @"^E\d{6}-\d{6}-\d{2}$";
+    private readonly string _value;
+
+    public OrderId(string orderId)
+    {
+        if (string.IsNullOrWhiteSpace(orderId))
+        {
+            throw new ValidationArgumentNullException("注文IDは必須です");
+        }
+
+        if (Regex.IsMatch(orderId, PATTERN) == false)
+        {
+            throw new ValidationArgumentException("注文IDのフォーマットが不正です");
+        }
+
+        _value = orderId;
+    }
+
+    /// <summary>
+    /// 注文ID
+    /// </summary>
+    public string Value => _value;
+}
---
+++ Akizuki.Domain.Test/Orders/OrderIdTest.cs
@@ -0,0 +1,43 @@
+﻿using Akizuki.Domain.Orders;
+using CapStore.Domain.Shareds.Exceptions;
+
+namespace Akizuki.Domain.Test.Orders;
+
+public class OrderIdTest
+{
+
+    [Theory(DisplayName = "注文ID nullテスト")]
+    [Trait("Category", "Akizuki")]
+    [InlineData(null)]
+    [InlineData("")]
+    public void NullTest(string? orderIdStr)
+    {
+        Assert.Throws<ValidationArgumentNullException>(() => new OrderId(orderIdStr));
+    }
+
+    [Theory(DisplayName = "注文ID nullテスト")]
+    [Trait("Category", "Akizuki")]
+    [InlineData("AAA")]
+    [InlineData("E230617-031873")]
+    [InlineData("E230617-031873-0")]
+    public void FormatFailTest(string orderIdStr)
+    {
+        Assert.Throws<ValidationArgumentException>(() => new OrderId(orderIdStr));
+    }
+
+    [Theory(DisplayName = "注文ID成功テスト")]
+    [Trait("Category", "Akizuki")]
+    [InlineData("E230617-031873-01")]
+    [InlineData("E230607-009435-01")]
+    [InlineData("E220706-100082-01")]
+    [InlineData("E220425-114327-01")]
+    [InlineData("E211028-104705-01")]
+    [InlineData("E211021-107294-01")]
+    [InlineData("E210705-044241-01")]
+    [InlineData("E201202-117662-01")]
+    public void SuccessTest(string orderIdStr)
+    {
+        OrderId orderId = new OrderId(orderIdStr);
+        Assert.Equal(orderIdStr, orderId.Value);
+    }
+}
