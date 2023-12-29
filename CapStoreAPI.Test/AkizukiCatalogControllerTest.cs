930c4a352b1e103e5d447987d492f1ab475adb0d
george <georgetech1984@gmail.com>
2023-12-29 21:54:59 +0900

    [WIP]電子部品情報登録用のシードデータ作成プログラム

--- CapStoreAPI.Test/AkizukiCatalogControllerTest.cs
+++ CapStoreAPI.Test/AkizukiCatalogControllerTest.cs
@@ -1,6 +1,12 @@
 ﻿using System.Net;
+using System.Text;
+using System.Linq;
+using Akizuki.Domain.Catalogs;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 namespace CapStoreAPI.Test;
 
@@ -34,5 +40,49 @@ public class AkizukiCatalogControllerTest : IClassFixture<PostgreSqlTest>, IDisp
         var jsonResponse = await response.Content.ReadAsStringAsync();
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+
+    [Fact(DisplayName = "電子部品登録json作成")]
+    [Trait("Category", "Akizuki")]
+    public async Task CreateComponentsJson()
+    {
+        IAsyncEnumerable<CatalogId> catalogIds = CreateCatalogIdsAsync();
+
+        // すべての言語セットをエスケープせずにシリアル化させる
+        // JsonSerializerOptions options = new JsonSerializerOptions()
+        // {
+        //     Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+        // };
+        IAsyncEnumerable<string> jsonList = catalogIds.SelectAwait(async x =>
+           {
+               using HttpResponseMessage response = await _httpClient.GetAsync($"/api/v1/akizuki/catalogs/{x.Value}");
+               return await response.Content.ReadAsStringAsync();
+               //return JsonSerializer.Serialize(responseStr, options);
+           });
+
+        const string PATH = "../../../../CapStoreAPI.Test/Assets/components.json";
+        using (StreamWriter writer = new StreamWriter(PATH, false, Encoding.UTF8))
+        {
+            await foreach (string json in jsonList)
+            {
+                await writer.WriteLineAsync(json);
+            }
+            await writer.FlushAsync();
+        }
+    }
+
+    public async IAsyncEnumerable<CatalogId> CreateCatalogIdsAsync()
+    {
+        const string PATH = "../../../../Akizuki.Infrastructure.Html.Test/Orders/Assets/order-catalogs.txt";
+        using (StreamReader reader = new StreamReader(PATH, Encoding.UTF8))
+        {
+            string? line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line)) { continue; }
+                yield return new CatalogId(line);
+            }
+        }
     }
 }
