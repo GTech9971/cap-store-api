0c802caa7bb39feba83e33e909f7ec93d050fc6c
George <georgetech1984@gmail.com>
2024-03-17 10:01:48 +0900

    ComponentTestをDomainsに移動

---
+++ CapStore.Domains.Tests/Components/ComponentIdTest.cs
@@ -0,0 +1,31 @@
+﻿namespace CapStore.Domains.Components.Test;
+
+using CapStore.Domains.Shareds.Exceptions;
+
+public class ComponentIdTest
+{
+    private const string CATEGORY = "電子部品ID";
+
+    [Theory]
+    [Trait("Category", CATEGORY)]
+    [InlineData(-1)]
+    public void TestExceptions(int id)
+    {
+        Assert.Throws<ValidationArgumentException>(() =>
+        {
+            new ComponentId(id);
+        });
+    }
+
+    [Theory]
+    [Trait("Category", CATEGORY)]
+    [InlineData(1)]
+    [InlineData(100)]
+    [InlineData(101)]
+    [InlineData(int.MaxValue)]
+    public void TestSuccess(int id)
+    {
+        var componentId = new ComponentId(id);
+        Assert.Equal(id, componentId.Value);
+    }
+}
