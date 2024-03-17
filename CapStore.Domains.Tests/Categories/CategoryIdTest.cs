ff8b74cefdcc92cdccda09f323fc83bf3bd3eca1
George <georgetech1984@gmail.com>
2024-03-17 09:55:55 +0900

    CategoryTestをDomainsに移動

---
+++ CapStore.Domains.Tests/Categories/CategoryIdTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CapStore.Domains.Categories;
+using CapStore.Domains.Shareds.Exceptions;
+
+namespace CapStore.Domains.Components.Test
+{
+	public class CategoryIdTest
+	{
+		private const string CATEGORY = "カテゴリーID";
+
+
+		[Theory]
+		[Trait("Category", CATEGORY)]
+		[InlineData(-1)]
+		public void TestExceptions(int id)
+		{
+			Assert.Throws<ValidationArgumentException>(() =>
+			{
+				new CategoryId(id);
+			});
+		}
+
+
+		[Theory]
+		[Trait("Category", CATEGORY)]
+		[InlineData(0)]
+		[InlineData(1)]
+		[InlineData(100)]
+		public void TestSuccess(int id)
+		{
+			var categoryId = new CategoryId(id);
+			Assert.Equal(id, categoryId.Value);
+		}
+	}
+}
+
---
+++ CapStore.Domains.Tests/Categories/CategoryNameTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CapStore.Domains.Categories;
+using CapStore.Domains.Shareds.Exceptions;
+
+namespace CapStore.Domains.Components.Test
+{
+    public class CategoryNameTest
+    {
+
+        private const string CATEGORY = "カテゴリー名";
+
+
+        [Fact]
+        [Trait("Category", CATEGORY)]
+        public void TestNullExceptions()
+        {
+            Assert.Throws<ValidationArgumentNullException>(() =>
+            {
+                new CategoryName(null!);
+            });
+        }
+
+        [Fact]
+        [Trait("Category", CATEGORY)]
+        public void TestWhiteSpaceExceptions()
+        {
+            Assert.Throws<ValidationArgumentNullException>(() =>
+            {
+                new CategoryName("");
+            });
+        }
+
+        [Theory]
+        [Trait("Category", CATEGORY)]
+        [InlineData("PIC")]
+        [InlineData("半導体")]
+        [InlineData("抵抗")]
+        [InlineData("LED・抵抗")]
+        public void TestSuccess(string name)
+        {
+            var categoryName = new CategoryName(name);
+            Assert.Equal(name, categoryName.Value);
+        }
+    }
+}
+
